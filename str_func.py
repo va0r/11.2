c7d6023ebe685a2386d8b8fb967b5596c43f5f8e
viktorlov <orlovva@ymail.com>
2023-04-19 17:57:10 +0300

    str_func.py code review

--- str_func.py
+++ str_func.py
@@ -1,5 +1,15 @@
-def str_func(arg: str) -> str:
+def str_func_1(arg: str) -> str:
     """
     upper
+    :param arg:
+    :return:
     """
     return arg.upper()
+
+def str_func_2(arg: str) -> str:
+    """
+    title
+    :param arg:
+    :return:
+    """
+    return arg.title()
